50a524a5124a96538cacf5bebc46b60be7aa4f79
martingg92 <42093381+martingg92@users.noreply.github.com>
2026-01-27 12:07:50 -0300

    Update oci_generative_ai_chat.py

--- app/services/oci_generative_ai_chat.py
+++ app/services/oci_generative_ai_chat.py
@@ -1,6 +1,7 @@
 import os
 
-from langchain_community.chat_models import ChatOCIGenAI
+#from langchain_community.chat_models import ChatOCIGenAI
+from langchain_oci import ChatOCIGenAI
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 
