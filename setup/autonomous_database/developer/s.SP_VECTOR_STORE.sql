2a864b6a27082dd0f277bde5443b55e2a35d07f9
martingg92 <42093381+martingg92@users.noreply.github.com>
2026-02-02 14:41:28 -0300

    Update s.SP_VECTOR_STORE.sql

--- setup/autonomous_database/developer/s.SP_VECTOR_STORE.sql
+++ setup/autonomous_database/developer/s.SP_VECTOR_STORE.sql
@@ -15,9 +15,9 @@
             CROSS JOIN dbms_vector_chain.utl_to_chunks(
                 a.TEXT,
                 json('{
-                    "by"        : "characters",
-                    "max"       : "512",
-                    "overlap"   : "51",
+                    "by"        : "words",
+                    "max"       : "200",
+                    "overlap"   : "40",
                     "split"     : "recursively",
                     "language"  : "'|| a.LANGUAGE ||'",
                     "normalize" : "all"
